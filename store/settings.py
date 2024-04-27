a488e033507bb8253a4382e1058f832f08d93150
Nikita Kuznetsov <cskuznetsov@gmail.com>
2024-04-27 10:18:25 +0300

    PostgreSQL connection, update products.html

--- store/settings.py
+++ store/settings.py
@@ -83,9 +83,13 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get('NAME_DB'),
+        "USER": os.environ.get('USER_DB'),
+        "PASSWORD": os.environ.get('PASSWORD_DB'),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
