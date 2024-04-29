cce92fb73e004b5c9d20b03129052d95f84c3172
Nikita Kuznetsov <cskuznetsov@gmail.com>
2024-04-29 10:58:01 +0300

    add tests

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,36 @@
+from http import HTTPStatus
+
 from django.test import TestCase
+from django.urls import reverse
+
+from products.models import Product, ProductCategory
 
 # Create your tests here.
+
+class IndexViewTestCase(TestCase):
+
+    def test_view(self):
+        path =  reverse('products:index')
+        response = self.client.get(path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store')
+        self.assertTemplateUsed(response, 'products/index.html')
+
+class ProductListViewTestCase(TestCase):
+    fixtures = ['categories.json', 'goods.json']
+
+    def setUp(self):
+        self.products = Product.objects.all()
+
+    def _common_tests(self, response):
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store - Каталог')
+        self.assertTemplateUsed(response, 'products/products.html')
+
+    def test_list(self):
+        path =  reverse('products:products')
+        response = self.client.get(path)
+        self._common_tests(response)
+
+        self.assertEqual(list(response.context_data['object_list']), list(self.products[:6]))
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from django.urls import reverse
+from http import HTTPStatus
+from .models import User, EmailVerification
+from django.utils.timezone import now
+from datetime import timedelta
 
 # Create your tests here.
+
+class UserRegistrationViewTestCase(TestCase):
+    fixtures = ['socialapp.json']
+
+    def setUp(self):
+        self.data = {
+            'first_name': 'testtesttest', 'last_name': 'testtesttest',
+            'username': 'testtesttest', 'email': 'test@test.test',
+            'password1': '12345678tT', 'password2': '12345678tT'
+        }
+        self.path = reverse('users:registration')
+
+    def _common_tests(self, response):
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store - Регистрация')
+        self.assertTemplateUsed(response, 'users/registration.html')
+
+    def test_user_registration_get(self):
+        response = self.client.get(self.path)
+
+    def test_user_registration_post_success(self):
+        username = self.data['username']
+        self.assertFalse(User.objects.filter(username=username).exists())
+        response = self.client.post(self.path, self.data)
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('users:login'))
+        self.assertTrue(User.objects.filter(username=username).exists())
+
+        email_verification = EmailVerification.objects.filter(user__username=username)
+        self.assertTrue(email_verification.exists())
+        self.assertEqual(
+            email_verification.first().expiration.date(),
+            (now() + timedelta(hours=48)).date()
+        )
+
+    def test_user_registration_post_error(self):
+        User.objects.create(username=self.data['username'])
+        response = self.client.post(self.path, self.data)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, 'Пользователь с таким именем уже существует.')
