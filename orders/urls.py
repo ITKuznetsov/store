ddb81e1712eb37a24a1c60f2ca52996274c743b7
Nikita Kuznetsov <cskuznetsov@gmail.com>
2024-05-02 14:23:58 +0300

    complete orders app (#)

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 app_name = 'orders'
 
 urlpatterns = [
-    path('order-create/', views.OrderCreateView.as_view(), name='order_create')
+    path('order-create/', views.OrderCreateView.as_view(), name='order_create'),
+    path('order-created/', views.OrderSuccessView.as_view(), name='success'),
+    path('', views.OrderListView.as_view(), name='orders'),
+    path('order', views.OrderView.as_view(), name='order'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,36 @@
 from django.views.generic.edit import CreateView
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 from orders.forms import OrderForm
+from orders.models import Order
 from django.urls import reverse_lazy
 from common.views import TitleMixin
 
 # Create your views here.
 
+class OrderListView(TitleMixin, ListView):
+    template_name = 'orders/orders.html'
+    title = 'Store - Заказы'
+    queryset = Order.objects.all()
+    ordering = ('-created')
+    
+    def get_queryset(self):
+        queryset = super(OrderListView, self).get_queryset()
+        return queryset.filter(initiator=self.request.user)
+    
+
+class OrderSuccessView(TitleMixin, TemplateView):
+    template_name = 'orders/success.html'
+    title = 'Store - Заказ успешно сформирован'
+
+class OrderView(TitleMixin, TemplateView):
+    template_name = 'orders/order.html'
+    title = 'Store - Заказ'
+
 class OrderCreateView(TitleMixin, CreateView):
     template_name = 'orders/order-create.html'
     form_class = OrderForm
-    success_url = reverse_lazy('orders:order_create')
+    success_url = reverse_lazy('orders:success')
     title = 'Store - Оформление заказа'
 
     def form_valid(self, form):
